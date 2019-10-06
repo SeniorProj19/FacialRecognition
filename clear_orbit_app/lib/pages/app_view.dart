d67bc67c02514fc0eedd39010ad01fa3e3f3fc9a
Andrew <andreww107@gmail.com>
2019-10-06 14:51:11 -0400

    Create app_view.dart
    added app view

---
+++ clear_orbit_app/lib/pages/app_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:clear_orbit_app/pages/camera_view.dart';
+import 'package:clear_orbit_app/pages/connections_view.dart';
+import 'package:clear_orbit_app/pages/card_view.dart';
+
+class app_view extends StatelessWidget {
+
+  final PageController controller = new PageController();
+  final List<Widget> widgetList = <Widget> [
+    connections_view(),
+    camera_view(),
+    card_view()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Weatherby Prototype'),
+      ),
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: <Widget>[
+          PageView.builder(
+              physics: ClampingScrollPhysics(),
+              itemCount: widgetList.length,
+              controller: controller,
+              itemBuilder: (context, index){
+                return widgetList[index];
+              }
+          ),
+
+
+        ],
+      ),
+    );
+
+  }
+}
