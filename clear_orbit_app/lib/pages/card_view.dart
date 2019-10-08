342502cb41f21f65ae305bf4672b3f70b8593d53
Andrew <andreww107@gmail.com>
2019-10-08 19:33:29 -0400

    Update card_view.dart
    Updated Card View

--- clear_orbit_app/lib/pages/card_view.dart
+++ clear_orbit_app/lib/pages/card_view.dart
@@ -8,10 +8,96 @@ class card_view extends StatefulWidget{
 class card_view_state extends State<card_view > {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text("This is the card view")
-      ],
+    return Container(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        color: Colors.black,
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  "AW",
+                style: TextStyle(fontSize: 70),
+                ),
+                radius: 100,
+              ),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Andrew Weatherby",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35
+                    ),
+                  ),
+                  Text(
+                    "Veracity Engineering",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25
+                    ),
+                  ),
+                  Text(
+                    "Summer-Fall Intern",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "LinkedIn",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25
+                    ),
+                  ),
+                  Text(
+                    "andrew-h-weatherby",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                  "Email",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25
+                  ),
+                  ),
+                  Text(
+                  "weatherba0@students.rowan.edu",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
