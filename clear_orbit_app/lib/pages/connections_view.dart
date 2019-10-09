d8715b3bcab62f946fad3b70f2be181a883a4259
Andrew <andreww107@gmail.com>
2019-10-08 20:41:30 -0400

    Update connections_view.dart
    Updated connections view. Contains hard coded accounts.

--- clear_orbit_app/lib/pages/connections_view.dart
+++ clear_orbit_app/lib/pages/connections_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clear_orbit_app/models/account.dart';
 
 class connections_view extends StatefulWidget{
   @override
@@ -6,7 +7,7 @@ class connections_view extends StatefulWidget{
 }
 
 class connections_view_state extends State<connections_view> {
-  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<Account> entries = getAccounts();
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +15,90 @@ class connections_view_state extends State<connections_view> {
       padding: const EdgeInsets.only(top: 20.0),
       itemCount: entries.length,
       itemBuilder: (BuildContext context, int index){
-        return Container(
-          height: 50,
-          color: Colors.blue,
-          child: Center(child: Text(entries[index]),),
+        return Card(
+          elevation: 8.0,
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration: (
+            BoxDecoration(color: Colors.black)),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  leading: Container(
+                    padding: EdgeInsets.only(right: 12.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      child: Text(
+                        entries[index].name_first.substring(0,1)+entries[index].name_last.substring(0,1),
+                      ),
+                    ),
+                  ),
+                  title: Text(entries[index].name_first + " " + entries[index].name_last,
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    entries[index].company + " - " + entries[index].title,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  trailing: Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0,),
+            ),
+          ),
         );
       },
       separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 
+}
+
+List<Account> getAccounts(){
+  return[
+    Account(
+      name_first:"Andrew",
+      name_last:"Weatherby",
+      company:"Veracity Engineering",
+      title: "Intern",
+      account_email: "weatherba0@students.rowan.edu",
+      account_linkedin: "andrew-weatherby"
+    ),
+    Account(
+        name_first:"Shafin",
+        name_last:"Siraj",
+        company:"Lockheed Martin",
+        title: "Manager",
+        account_email: "sirajs8@students.rowan.edu",
+        account_linkedin: "shafin-siraj"
+    ),
+    Account(
+        name_first:"Tyler",
+        name_last:"Fung",
+        company:"NASA",
+        title: "Programmer",
+        account_email: "fungt5@students.rowan.edu",
+        account_linkedin: "tyler-fung"
+    ),
+    Account(
+        name_first:"Dan",
+        name_last:"Vega",
+        company:"Google",
+        title: "Programmer",
+        account_email: "vegad8@students.rowan.edu",
+        account_linkedin: "dan-vega"
+    ),
+    Account(
+        name_first:"EJ",
+        name_last:"Pellot",
+        company:"Google",
+        title: "Programmer",
+        account_email: "edwinpellot0110@gmail.com",
+        account_linkedin: "E-J"
+    ),
+    Account(
+        name_first:"Vince",
+        name_last:"Williams",
+        company:"Google",
+        title: "Programmer",
+        account_email: "williamsv8@students.rowan.edu",
+        account_linkedin: "vince-williams"
+    ),
+  ];
 }
