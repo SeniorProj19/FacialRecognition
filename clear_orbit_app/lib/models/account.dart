5476faef0d4229b2b5da0110982156a52abc4710
Andrew <andreww107@gmail.com>
2019-10-08 20:40:58 -0400

    Create account.dart
    added account model

---
+++ clear_orbit_app/lib/models/account.dart
@@ -0,0 +1,11 @@
+class Account {
+  String name_first;
+  String name_last;
+  String company;
+  String title;
+  String account_linkedin;
+  String account_email;
+
+  Account(
+      {this.name_first, this.name_last, this.company, this.title, this.account_email, this.account_linkedin});
+}
