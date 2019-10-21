a320c0256f8dccc8c821ec455f6dd66a465c9c4c
Andrew <andreww107@gmail.com>
2019-10-21 12:29:26 -0400

    Update card_view.dart

--- clear_orbit_app/lib/pages/card_view.dart
+++ clear_orbit_app/lib/pages/card_view.dart
@@ -1,12 +1,20 @@
 import 'package:clear_orbit_app/services/global.dart';
 import 'package:flutter/material.dart';
+import 'package:clear_orbit_app/models/account.dart';
 
 class card_view extends StatefulWidget{
   @override
   card_view_state createState() => card_view_state();
 }
 
+class CardArguments {
+  final Account account;
+
+  CardArguments(this.account);
+}
+
 class card_view_state extends State<card_view > {
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,7 +28,7 @@ class card_view_state extends State<card_view > {
               child: CircleAvatar(
                 backgroundColor: Colors.blue,
                 child: Text(
-                  account_viewing.getInitials(),
+                  account_signedin.getInitials(),
                 style: TextStyle(fontSize: 70),
                 ),
                 radius: 100,
@@ -30,21 +38,21 @@ class card_view_state extends State<card_view > {
               child: Column(
                 children: <Widget>[
                   Text(
-                    account_viewing.name_first + " " + account_viewing.name_last,
+                    account_signedin.name_first + " " + account_signedin.name_last,
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 35
                     ),
                   ),
                   Text(
-                    account_viewing.company,
+                    account_signedin.company,
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 25
                     ),
                   ),
                   Text(
-                    account_viewing.title,
+                    account_signedin.title,
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 25
@@ -66,7 +74,7 @@ class card_view_state extends State<card_view > {
                     ),
                   ),
                   Text(
-                    account_viewing.account_linkedin,
+                    account_signedin.account_linkedin,
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 20
@@ -87,7 +95,7 @@ class card_view_state extends State<card_view > {
                   ),
                   ),
                   Text(
-                  account_viewing.account_email,
+                    account_signedin.account_email,
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 20
