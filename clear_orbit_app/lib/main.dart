050b02ab1f5761adb39ba17a24902df2f99343c1
Andrew <andreww107@gmail.com>
2019-10-28 13:09:16 -0400

    Update main.dart
    removed debug banner

--- clear_orbit_app/lib/main.dart
+++ clear_orbit_app/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Clear Orbit Prototype',
       theme: ThemeData(
         primarySwatch: Colors.blue,
