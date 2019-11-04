56919454c24b7e0f765607e3d48cf1b138a730e7
Andrew <andreww107@gmail.com>
2019-11-03 20:55:06 -0500

    Update sign_in_page.dart
    made major changes used for new Flask support

--- clear_orbit_app/lib/pages/sign_in_page.dart
+++ clear_orbit_app/lib/pages/sign_in_page.dart
@@ -1,85 +1,107 @@
+import 'package:clear_orbit_app/services/API.dart';
 import 'package:flutter/material.dart';
 import 'package:clear_orbit_app/services/account_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'dart:convert' as convert;
+//import 'API.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends API {
   @override
-  _ListPageState createState() {
-    return _ListPageState();
-  }
+  LoginPageState createState() => LoginPageState();
 }
 
-class _ListPageState extends State<LoginPage> {
-  TextEditingController  usernameController = new TextEditingController(); 
-  TextEditingController  passwordController = new TextEditingController();
-  bool _password = false;
+class LoginPageState extends APIState {
+
+
+  String URL;
+  String URLSuffix = "mlogin";
+  String appBarTitle = "Login";
+
+  var messages = {
+    "success" : {"dialogHead":"","dialogMessage":""} ,
+    "failure" : {"dialogHead" : "Login error"}
+  } ;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        children: <Widget>[
-          SizedBox(height: 80.0),
-          Column(
+  void onDialogPressed() {
+    if(success == false)
+      Navigator.pop(context);
+  }
+
+  @override
+  onSuccess() async {
+    // When login success, save the data ,
+
+    sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool("isLogged", true);
+
+    print("Printing Statement: " + responseData["data"]["user"]); // Where is response data coming from
+
+    sharedPreferences.setString("username", responseData["data"]["user"]);
+    //sharedPreferences.setString("fullname", responseData["data"]["user"]["fullname"]);
+    //sharedPreferences.setString("email", responseData["data"]["user"]["email"]);
+
+    // navigate to ProfilePage
+    Navigator.pushReplacementNamed(context, '/main') ;
+  }
+
+  @override
+  Map<String, String> getBody() {
+    return {"username": username, "password": password};
+  }
+
+  @override
+  Widget getForm() {
+    return new Form(
+        key: formKey,
+        child: Container(
+          padding: const EdgeInsets.all(32.0),
+          child: new Column(
             children: <Widget>[
-              Image.asset("assets/lock.png", scale: 1.5),
-              SizedBox(
-                height: 30.0,
+              SizedBox(height: 80.0),
+              Column(
+                children: <Widget>[
+                  Image.asset("assets/lock.png", scale: 1.5),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Text(
+                    'Clear Orbit',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  )
+                ],
               ),
-              Text(
-                'Clear Orbit',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-              )
+              SizedBox(height: 20.0),
+              new SizedBox(
+                height: 80.0,
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: "Username"),
+                  validator: (value) {
+                    return value.isNotEmpty ? null : "Empty Username";
+                  },
+                  onSaved: (val) => username = val,
+                ),
+              ),
+              new SizedBox(
+                height: 90.0,
+                child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Password"),
+                    validator: (value) {
+                      return value.isNotEmpty ? null : "Empty Password";
+                    },
+                    onSaved: (val) => password = val),
+              ),
+              new FlatButton(
+                onPressed: super.submit ,
+                child: Text('Sign In', style: TextStyle(color: Colors.white),),
+                color: Color.fromRGBO(46, 108, 164, 1),
+              ),
             ],
           ),
-          SizedBox(height: 75.0),
-          Visibility(
-            visible: _password,
-            child: Padding(
-            padding: EdgeInsets.only(bottom: 15.0),
-            child: Text(
-              "Username or Password is incorrect",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.red, fontSize: 17),
-            ),
-          ),),
-          TextField(
-            controller: usernameController,
-            decoration: InputDecoration(
-              labelText: 'Username',
-              filled: true,
-            ),
-          ),
-          SizedBox(height: 12.0),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              filled: true,
-            ),
-            obscureText: true,
-          ),
-          FlatButton(
-            child: Text('Sign In', style: TextStyle(color: Colors.white),),
-            color: Color.fromRGBO(46, 108, 164, 1),
-            onPressed: () async {
-              Post newPost = new Post(
-                username: this.usernameController.text,
-                password: this.passwordController.text);
-              await fetchPost(body: newPost.toMap()); // Method that calls for the data - EJ
-              //if login from status message go to next screen
-              
-              Navigator.pushNamedAndRemoveUntil(
-                  context, '/main', (Route<dynamic> route) => false);
-              // } else
-              // setState(() {
-              //   _password = false;
-              // });
-            },
-          )
-        ],
-      ),
-    ));
+        )
+    );
   }
+
 }
