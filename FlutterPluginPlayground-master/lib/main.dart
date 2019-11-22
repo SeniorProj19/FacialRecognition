fca64a475948489bc0bc3f4f781d4379288267bc
Andrew <andreww107@gmail.com>
2019-11-21 23:02:41 -0500

    Main swap

--- FlutterPluginPlayground-master/lib/main.dart
+++ FlutterPluginPlayground-master/lib/main.dart
@@ -1,72 +1,31 @@
+import 'package:flutter_plugin_playground/pages/card_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_plugin_playground/pages/app_view.dart';
+import 'package:flutter_plugin_playground/pages/sign_in_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-const channel = const MethodChannel('playground');
+void main() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLogged = (prefs.getBool('isLogged') ?? false);
 
-void main() => runApp(PlaygroundApp());
+  var home;
+  if(isLogged)
+    home = app_view();
+  else
+    home = LoginPage() ;
 
-class PlaygroundApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Plugin Playground',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Playground(title: 'Plugin Playground'),
-    );
-  }
-}
-
-class Playground extends StatefulWidget {
-  Playground({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  PlaygroundState createState() => PlaygroundState();
-}
-
-class PlaygroundState extends State<Playground> {
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Press button to run playground"),
-            Text("-"),
-            Text(logs)
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: runPlayground,
-        tooltip: 'Run Playground',
-        child: Icon(Icons.play_arrow),
-      )
-    );
-  }
-
-  /////////////// Playground ///////////////////////////////////////////////////
-  String logs = "";
-
-  // Call inside a setState({ }) block to be able to reflect changes on screen
-  void log(String logString) {
-    logs += logString.toString() + "\n";
-  }
-
-  // Main function called when playground is run
-  void runPlayground() async {
-    var testResult = await channel.invokeMethod("test");
-
-    setState(() {
-      log(testResult);
-    });
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Clear Orbit Prototype',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: home,
+    routes:{
+      '/login':(context) => LoginPage(),
+      '/main':(context) => app_view(),
+      '/card':(context) => new card_view(),
+    },
+  )
+  );
 }
